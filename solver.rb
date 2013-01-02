1fbf35e3d03a7ec237fde2c4b90be763556ba942
Lamont Lucas <lamont@fastrobot.com>
2013-01-01 22:05:54 -0800

    fleshing out solver comments.

--- solver.rb
+++ solver.rb
@@ -1,5 +1,8 @@
 class Solver
-  # methods for solving a sudoko quiz that I know, to be applied in any order
+  # methods for solving a sudoko quiz that I know
 
+  # simplify! does the most basic solution in Board that solves cells with only one possible answer
+
+  # guess_at_two
 
 end
